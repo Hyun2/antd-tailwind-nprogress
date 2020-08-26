3470440080c882b7c0fe3c922497e316a54a87a2
Hyun2 <hks0284@gmail.com>
2020-08-26 16:05:06 +0900

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "nprogress/nprogress.css";
+import Nprogress from "nprogress";
+import Router from "next/router";
+import "../styles/antd.less";
+
+Router.events.on("routeChangeStart", () => Nprogress.start());
+Router.events.on("routeChangeComplete", () => Nprogress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
